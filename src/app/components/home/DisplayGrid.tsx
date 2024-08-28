a72e37297e107a6bef9cbf1b427297093872e9ae
aprilslis <s826li@uwaterloo.ca>
2024-08-27 19:34:46 -0700

    styling the boards on start screen

--- src/app/components/home/DisplayGrid.tsx
+++ src/app/components/home/DisplayGrid.tsx
@@ -1,54 +1,54 @@
-interface DisplayGridProps {
-    className: string;
+export enum Color {
+  Coffee = "brown-1",
+  Chocolate = "brown-2",
+  Caramel = "brown-3",
+  Latte = "brown-4",
 }
 
-export const DisplayGrid = ({ className }: DisplayGridProps) => {
-    const capitalize = (str: string) => str.toUpperCase();
-    const grid = [
-        ["a", "b", "c", "d", "e"],
-        ["f", "g", "h", "i", "j"],
-        ["k", "l", "m", "n", "o"],
-        ["p", "q", "r", "s", "t"],
-        ["", "z", "w", "x", "v"],
-    ].map((row) => row.map((cell) => (cell === "" ? "" : capitalize(cell))));
+interface DisplayGridProps {
+  className: string;
+  outerBorder: Color;
+  topRowWords?: Color;
+  allWords: Color;
+}
 
-    const border: Record<string, string> = {
-        "00": "border-t-0 border-l-0 rounded-tl-3xl",
-        "01": "border-t-0",
-        "02": "border-t-0",
-        "03": "border-t-0",
-        "04": "border-t-0 border-r-0 rounded-tr-3xl",
-        "10": "border-l-0",
-        "20": "border-l-0",
-        "30": "border-l-0",
-        "40": "border-l-0 border-b-0 rounded-bl-3xl",
-        "41": "border-b-0",
-        "42": "border-b-0",
-        "43": "border-b-0",
-        "44": "border-b-0 border-r-0 rounded-br-3xl",
-        "14": "border-r-0",
-        "24": "border-r-0",
-        "34": "border-r-0",
-        default: "",
-    };
+export const DisplayGrid = ({
+  className,
+  outerBorder,
+  topRowWords,
+  allWords,
+}: DisplayGridProps) => {
+  const getWordColor = (index: number) => {
+    if (index === 0 && topRowWords) return `text-${topRowWords}`;
+    return `text-${allWords}`;
+  };
+  const capitalize = (str: string) => str.toUpperCase();
+  const grid = [
+    ["a", "b", "c", "d", "e"],
+    ["f", "g", "h", "i", "j"],
+    ["k", "l", "m", "n", "o"],
+    ["p", "q", "r", "s", "t"],
+    ["", "z", "w", "x", "v"],
+  ].map((row) => row.map((cell) => (cell === "" ? "" : capitalize(cell))));
 
-    return (
-        <div className={`border-2 h-fit bg-brown-4 rounded-3xl ${className}`}>
-            {grid.map((row, i) => (
-                <div key={i} className="flex">
-                    {row.map((cell, j) => (
-                        <div
-                            key={j}
-                            className={`border bg-secondary-background border-brown-2 flex items-center justify-center w-20 h-20 text-5xl 
-                                      ${border[`${i}${j}`]}  ${
-                                cell === "" && `!bg-brown-4`
-                            }`}
-                        >
-                            {cell}
-                        </div>
-                    ))}
-                </div>
-            ))}
+  return (
+    <div
+      className={`overflow-hidden border-2 border-${outerBorder} h-fit bg-brown-4 rounded-3xl ${className}`}
+    >
+      {grid.map((row, i) => (
+        <div key={i} className="flex">
+          {row.map((cell, j) => (
+            <div
+              key={j}
+              className={`border ${getWordColor(
+                i
+              )} bg-secondary-background border-brown-2 flex items-center justify-center w-28 h-28 text-7xl`}
+            >
+              {cell}
+            </div>
+          ))}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
--- src/app/components/home/StartGame.tsx
+++ src/app/components/home/StartGame.tsx
@@ -1,13 +1,26 @@
-import { DisplayGrid } from "./DisplayGrid";
+import { Color, DisplayGrid } from "./DisplayGrid";
 
 import styles from "./StartGame.module.scss";
 
 export const StartGame = () => {
-    return (
-        <div>
-            <DisplayGrid className={`${styles["front-grid"]}`} />
-            <DisplayGrid className={`${styles["middle-grid"]}`} />
-            <DisplayGrid className={`${styles["back-grid"]}`} />
-        </div>
-    );
+  return (
+    <div>
+      <DisplayGrid
+        className={`${styles["front-grid"]}`}
+        outerBorder={Color.Coffee}
+        topRowWords={Color.Coffee}
+        allWords={Color.Latte}
+      />
+      <DisplayGrid
+        className={`${styles["middle-grid"]}`}
+        outerBorder={Color.Chocolate}
+        allWords={Color.Caramel}
+      />
+      <DisplayGrid
+        className={`${styles["back-grid"]}`}
+        outerBorder={Color.Chocolate}
+        allWords={Color.Chocolate}
+      />
+    </div>
+  );
 };
